d8a0bd75628275b77cfd7fd78685a3948fe285bf
Thomas <thomasburfinp@gmail.com>
2025-02-09 18:08:47 +0100

    debug ve

--- public/main.js
+++ public/main.js
@@ -262,7 +262,7 @@ async function vehicleSetup() {
             variables: { page: 0, size: 20 },
         })
     }).then(async (res) => {
-        console.log(res.data);
+        if (res.errors) console.log(errors);
         const vehicles = (await res.json()).data.vehicleList;
 
         const vList = document.getElementById("vehicleList");
